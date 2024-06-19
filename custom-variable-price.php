6b8587ae9ee33856dcd77b9cae3669a52cd8545d
Mayank Kumar <96015005+markmemayank@users.noreply.github.com>
2024-06-19 19:06:00 +0530

    Update custom-variable-price.php

--- custom-variable-price.php
+++ custom-variable-price.php
@@ -1,34 +1,19 @@
-<?php
-/*
-Plugin Name: Custom Variable Price
-Description: Show the lowest price for variable products and update the price dynamically when a variation is selected.
-Version: 1.0
-Author: Mayank Kumar
-*/
+function wc_custom_variable_product_price($price, $product) {
+    if ($product->is_type('variable')) {
+        $available_variations = $product->get_available_variations();
+        $prices = array();
 
-// Show the lowest price for variable products
-add_filter( 'woocommerce_variable_sale_price_html', 'custom_woocommerce_variable_price', 10, 2 );
-add_filter( 'woocommerce_variable_price_html', 'custom_woocommerce_variable_price', 10, 2 );
+        foreach ($available_variations as $variation) {
+            $variation_obj = new WC_Product_Variation($variation['variation_id']);
+            $prices[] = $variation_obj->get_price();
+        }
 
-function custom_woocommerce_variable_price( $price, $product ) {
-    $min_price = $product->get_variation_price( 'min', true );
-    $min_price_sale = $product->get_variation_sale_price( 'min', true );
-
-    if ( $min_price_sale < $min_price ) {
-        $price = wc_price( $min_price_sale );
-    } else {
-        $price = wc_price( $min_price );
+        if (!empty($prices)) {
+            $min_price = min($prices);
+            $price = wc_price($min_price);
+        }
     }
-
-    $price .= $product->get_price_suffix();
 
     return $price;
 }
-
-// Enqueue the custom JavaScript
-function custom_enqueue_scripts() {
-    if ( is_product() ) {
-        wp_enqueue_script( 'custom-variation-price', plugin_dir_url( __FILE__ ) . 'js/custom-variation-price.js', array( 'jquery' ), '1.0', true );
-    }
-}
-add_action( 'wp_enqueue_scripts', 'custom_enqueue_scripts' );
+add_filter('woocommerce_get_price_html', 'wc_custom_variable_product_price', 10, 2);
